9a14d5b662715d3baedb861ffbce3430ee076c5f
limin5120 <112102135+limin5120@users.noreply.github.com>
2024-10-14 11:27:53 +0800

    Create __init__.py

---
+++ LogFun/core/__init__.py
@@ -0,0 +1,6 @@
+from .config import *
+from .utils import *
+from .logger import Logger, add_logger_to
+from .coreFunction import make_trace_function
+from .coreClass import install_trace_methods
+from .socket import setup_socket, save_to_server_socket
